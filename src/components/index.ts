0fff167b55c783a8dc75abd0df5146256c4f8b83
Sol-Zeta <mspattoglio@gmail.com>
2022-04-14 12:58:43 +0200

    update components index

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import { CardList } from './CardList';
+import { ProductCard } from './ProductCard';
+import { MainBackground } from './MainBackground';
+
+export { CardList, MainBackground, ProductCard }
