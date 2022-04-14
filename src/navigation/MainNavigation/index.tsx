403e6b4ffb5e6851f9aacfcc7d7942311e32b603
Sol-Zeta <mspattoglio@gmail.com>
2022-04-14 12:59:05 +0200

    add screens to navigation

--- src/navigation/MainNavigation/index.tsx
+++ src/navigation/MainNavigation/index.tsx
@@ -1,33 +1,31 @@
 import React from 'react';
+import {Text} from 'react-native'
 import {
   NavigationContainer,
   useNavigationContainerRef,
-} from "@react-navigation/native";
-import {
-  createStackNavigator
-} from '@react-navigation/stack';
-import { MainBackground } from '../../components/MainBackground';
-import { MainNavigationParams } from '../../interfaces';
-import { styles } from './styles'
+} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Home, Onboarding, Splash} from '../../screens';
+import {MainNavigationParams} from '../../interfaces';
+import {styles} from './styles';
 
 const MainNavigation: () => JSX.Element = () => {
-  const Stack = createStackNavigator<MainNavigationParams>();
+  const Stack = createStackNavigator();
   const navigationRef = useNavigationContainerRef<any>();
   return (
-    <MainBackground>
-     <NavigationContainer ref={navigationRef}>
-      <Stack.Navigator
-        screenOptions={{
-          animationEnabled: true,
-          gestureEnabled: false,
-        }}
-      >
-        <Stack.Screen name="splash" component={Splash} />
-        <Stack.Screen name="onboarding" component={Onboarding} />
-        <Stack.Screen name="home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
-    </MainBackground>
+      <NavigationContainer ref={navigationRef}>
+        <Stack.Navigator
+          initialRouteName="home"
+          screenOptions={{
+            animationEnabled: true,
+            gestureEnabled: false,
+            headerShown: false
+          }}>
+          <Stack.Screen name="splash" component={Splash} />
+          <Stack.Screen name="onboarding" component={Onboarding} />
+          <Stack.Screen name="home" component={Home}/>
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 };
 
--- src/navigation/MainNavigation/styles.ts
+++ src/navigation/MainNavigation/styles.ts
@@ -1,4 +1,4 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 import { colors } from '../../styles/common';
 
 export const styles = StyleSheet.create({
