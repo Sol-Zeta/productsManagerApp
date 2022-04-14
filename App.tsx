34e14b34c9397a8ac5e0f4504847f7ba7358849b
Sol-Zeta <mspattoglio@gmail.com>
2022-04-14 12:59:39 +0200

    fix App

---
+++ App.tsx
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import 'react-native-gesture-handler';
+import React from 'react';
+import { MainNavigation } from './src/navigation'
+
+const App: () => JSX.Element = () => {
+
+  return (
+    <MainNavigation />
+  );
+};
+
+
+export default App;
