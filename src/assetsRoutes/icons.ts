4ee2e03d5279e22d9e38713813bf8cc08a76edb0
Sol-Zeta <mspattoglio@gmail.com>
2022-04-14 13:46:46 +0200

    creat icons routes

---
+++ src/assetsRoutes/icons.ts
@@ -0,0 +1,10 @@
+export const icons = {
+    alert: require('../assets/icons/alert.svg'),
+    back: require('../assets/icons/back.svg'),
+    close: require('../assets/icons/close.svg'),
+    delete: require('../assets/icons/delete.svg'),
+    edit: require('../assets/icons/edit.svg'),
+    forward: require('../assets/icons/forward.svg'),
+    marked: require('../assets/icons/marked.svg'),
+    unmarked: require('../assets/icons/unmarked.svg')
+}
