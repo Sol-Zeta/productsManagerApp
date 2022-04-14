0fc22a5f2f9f29e7f298a57608461748659eaafa
Sol-Zeta <mspattoglio@gmail.com>
2022-04-14 22:18:42 +0200

    add new interfaces

--- src/assetsRoutes/icons.ts
+++ src/assetsRoutes/icons.ts
@@ -1,10 +1,12 @@
 export const icons = {
-    alert: require('../assets/icons/alert.svg'),
-    back: require('../assets/icons/back.svg'),
-    close: require('../assets/icons/close.svg'),
-    delete: require('../assets/icons/delete.svg'),
-    edit: require('../assets/icons/edit.svg'),
-    forward: require('../assets/icons/forward.svg'),
-    marked: require('../assets/icons/marked.svg'),
-    unmarked: require('../assets/icons/unmarked.svg')
+    alert: require('../assets/icons/alert.png'),
+    back: require('../assets/icons/back.png'),
+    close: require('../assets/icons/close.png'),
+    delete: require('../assets/icons/delete.png'),
+    edit: require('../assets/icons/edit.png'),
+    forward: require('../assets/icons/forward.png'),
+    marked: require('../assets/icons/marked.png'),
+    search: require('../assets/icons/search.png'),
+    share: require('../assets/icons/share.png'),
+    unmarked: require('../assets/icons/unmarked.png')
 }
--- src/assetsRoutes/images.ts
+++ src/assetsRoutes/images.ts
@@ -1,3 +1,10 @@
 export const images = {
-    card_picture: {uri: 'https://pixabay.com/photos/succulents-flower-plant-garden-939919/'}
+    card_picture: {uri: 'https://pixabay.com/photos/succulents-flower-plant-garden-939919/'},
+    carrot: require('../assets/images/products/carrot.png'),
+    lettuce: require('../assets/images/products/lettuce.png'),
+    onion: require('../assets/images/products/onion.png'),
+    pepper: require('../assets/images/products/pepper.png'),
+    tomato: require('../assets/images/products/tomato.png'),
+    cucumber: require('../assets/images/products/cucumber.png'),
+    potato: require('../assets/images/products/potato.png'),
 }
