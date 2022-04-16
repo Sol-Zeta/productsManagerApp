b1f8f0564d9bf6dbd222e825395c7fd2d8ba1009
Sol-Zeta <mspattoglio@gmail.com>
2022-04-16 20:59:34 +0200

    fix configuration

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Provider } from 'react-redux';
 import {MainNavigation, BottomBar} from './src/navigation';
 import store from './src/redux/configureStore'
 
 const App: () => JSX.Element = () => {
+
   return (
     <Provider store={store}>
       <MainNavigation />
