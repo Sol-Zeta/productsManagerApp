296e76c4ae2d0576c2f60045b7b3ebcb7383c8a0
Sol-Zeta <mspattoglio@gmail.com>
2022-04-16 14:31:40 +0200

    add new interfaces

--- src/interfaces/MainNavigationParams.ts
+++ src/interfaces/MainNavigationParams.ts
@@ -4,6 +4,7 @@ export type MainNavigationParams = {
   splash: undefined;
   onboarding: undefined;
   home: undefined;
+  navigation?: undefined;
   productDetail: { itemId: string},
   editProduct: { itemData: IProduct}
 };
