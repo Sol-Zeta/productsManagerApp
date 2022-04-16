552cb5f1ce65ac214432f190f833eeb85f23d85f
Sol-Zeta <mspattoglio@gmail.com>
2022-04-16 14:31:57 +0200

    create new util function

--- src/utils/calculatePages.ts
+++ src/utils/calculatePages.ts
@@ -1,2 +1,2 @@
 export const calculatePages = (items: number, itemsPerPage: number) =>
-  items % itemsPerPage === 0 ? items / itemsPerPage : Math.floor(items / itemsPerPage + 1);
+  items % itemsPerPage === 0 ? items / itemsPerPage : Math.floor(items / itemsPerPage);
