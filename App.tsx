d3780188629a093711401279d27ace5645afc105
Sol-Zeta <mspattoglio@gmail.com>
2022-04-16 14:29:46 +0200

    install packages

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { Provider } from 'react-redux';
-import {MainNavigation} from './src/navigation';
+import {MainNavigation, BottomBar} from './src/navigation';
 import store from './src/redux/configureStore'
 
 const App: () => JSX.Element = () => {
