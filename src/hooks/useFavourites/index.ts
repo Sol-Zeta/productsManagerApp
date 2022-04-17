9265ff4a7d4730573ef08ad56df4b76cb90c57f1
Sol-Zeta <mspattoglio@gmail.com>
2022-04-17 16:28:01 +0200

    disable hook

--- src/hooks/useFavourites/index.ts
+++ src/hooks/useFavourites/index.ts
@@ -9,82 +9,73 @@ export const useFavourites = () => {
   const {setItem, removeItem, getItem} = useAsyncStorage('favourites');
 
   const setFavouriteItem = async (item: string) => {
-      console.log("agregar a favoritos", item)
-      setIsLoading(true)
-    try {
-      const itemsToStore = JSON.stringify([...allFavs, item]);
-      await setItem(itemsToStore);
-      setAllFavs([...allFavs, item]);
-    } catch (error) {
-      console.error('Error writting local storage', error);
-    } finally {
-        setIsLoading(false)
-    }
+    //   setIsLoading(true)
+    // try {
+    //   const itemsToStore = JSON.stringify([...allFavs, item]);
+    //   await setItem(itemsToStore);
+    //   setAllFavs([...allFavs, item]);
+    // } catch (error) {
+    //   console.error('Error writting local storage', error);
+    // } finally {
+    //     setIsLoading(false)
+    // }
   };
 
   const getAllFavourites = async () => {
-    setIsLoading(true)
-    try {
-      const response = await getItem();
-      console.log('get fav', response);
-      if (response) {
-        const data = await JSON.parse(response);
-        setAllFavs(data);
-      } else {
-        setAllFavs([]);
-      }
-    } catch (error) {
-      console.error('Error reading local storage', error);
-      setAllFavs([]);
-    } finally {
-        setIsLoading(false)
-    }
+    // setIsLoading(true)
+    // try {
+    //   const response = await getItem();
+    //   if (response) {
+    //     const data = await JSON.parse(response);
+    //     setAllFavs(data);
+    //   } else {
+    //     setAllFavs([]);
+    //   }
+    // } catch (error) {
+    //   console.error('Error reading local storage', error);
+    //   setAllFavs([]);
+    // } finally {
+    //     setIsLoading(false)
+    // }
   };
 
   const removeFavourite = async (item: string) => {
-    setIsLoading(true)
-    try {
-      if (allFavs.length) {
-        const newFavourites = allFavs.filter((e: string) => e !== item);
-        await setItem(JSON.stringify(newFavourites));
-        setAllFavs(newFavourites);
-        console.log('nuevos favs despues de borrar', newFavourites)
-      }
-    } catch (error) {
-      console.error('Error deleting local storage');
-    } finally {
-        setIsLoading(false)
-    }
+    // setIsLoading(true)
+    // try {
+    //   if (allFavs.length) {
+    //     const newFavourites = allFavs.filter((e: string) => e !== item);
+    //     await setItem(JSON.stringify(newFavourites));
+    //     setAllFavs(newFavourites);
+    //   }
+    // } catch (error) {
+    //   console.error('Error deleting local storage');
+    // } finally {
+    //     setIsLoading(false)
+    // }
   };
 
   useEffect(() => {
-    getAllFavourites();
+    // getAllFavourites();
   }, []);
 
   useEffect(() => {
-      console.log('hola effect is loading', isLoading)
-    if (!isLoading && newFav !== '') {
-        console.log('setting favouriteitem')
-      setFavouriteItem(newFav);
-    }
+    // if (!isLoading && newFav !== '') {
+    //   setFavouriteItem(newFav);
+    // }
   }, [newFav]);
-
-  useEffect(() => {
-    console.log("loading", isLoading)
-  }, [isLoading])
-  
   
 
-  const removeFavID = (id: string) => removeFavourite(id);
+  const removeFavID = (id: string) => {};
+//   const removeFavID = (id: string) => removeFavourite(id);
   const addFavID = (id: string) => {
-    if (!allFavs.includes(id)) {
-        setNewFav(id);
-        console.log("agregando id", id)
-    }
+    // if (!allFavs.includes(id)) {
+    //     setNewFav(id);
+    // }
   };
 
   const isFavID = (id: string) => {
-      return allFavs.includes(id)
+      return false
+    //   return allFavs.includes(id)
     };
 
   return {removeFavID, addFavID, allFavs, isFavID};
